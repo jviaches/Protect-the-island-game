d485ce22bfe1eab30f52933a354c6a0030ed1eec
jviaches <jviaches@gmail.com>
2019-01-12 11:44:18 -0600

    Coins collection movement (Issue #5)

--- AirBaloons/Assets/Script/Collectables/CoinScript.cs
+++ AirBaloons/Assets/Script/Collectables/CoinScript.cs
@@ -2,21 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinScript : MonoBehaviour {
+public class CoinScript : MonoBehaviour
+{
+
+    private const float speed = 20f;
+    private bool allowedToBeCollected;
+    private bool isCointAlreadyCounted;
+
+    private GameObject itemsCollectedObject;
+
+    void Start()
+    {
+        itemsCollectedObject = GameObject.Find("ItemsCollectionObject");
+    }
+
+    void Update()
+    {
+        if (allowedToBeCollected)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, itemsCollectedObject.transform.position, step);
+        }
+        else
+        {
+            Invoke("destroyCoin", 3f);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "ItemsCollectionObject")
+            Destroy(gameObject);
+    }
 
     void OnMouseDown()
     {
+        if (!isCointAlreadyCounted)
+            coinCounted(1);
+    }
+
+    private void coinCounted(int bonus = 0)
+    {
         GetComponent<AudioSource>().Play();
 
-        PlayerScript player = GameObject.Find("Player").GetComponent<PlayerScript>();
-        player.CoinsUpdate(1);
+        //GetComponent<Animator>().SetBool("Pulse", true);
+        //GetComponent<Animator>().PlayInFixedTime("Pulsation", 0, 1f);
 
-        Invoke("destroyCoin", 0.07f);
-        
+
+        GameObject.Find("Player").GetComponent<PlayerScript>().CoinsUpdate(1 + bonus);
+
+        isCointAlreadyCounted = true;
+        allowedToBeCollected = true;
     }
 
     private void destroyCoin()
     {
-        Destroy(gameObject);
+        if (!isCointAlreadyCounted)
+            coinCounted();
     }
 }
