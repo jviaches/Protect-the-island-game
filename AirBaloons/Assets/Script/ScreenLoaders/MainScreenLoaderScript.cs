9b07c1dffb8b7161ecb625691eb393be00cda9d7
jviaches <jviaches@gmail.com>
2019-01-12 12:24:36 -0600

    Add sound screen (issue #6)

--- AirBaloons/Assets/Script/ScreenLoaders/MainScreenLoaderScript.cs
+++ AirBaloons/Assets/Script/ScreenLoaders/MainScreenLoaderScript.cs
@@ -6,12 +6,15 @@ public class MainScreenLoaderScript : MonoBehaviour {
 
     private Button exitButton;
     private Button playButton;
+    private Button settingsButton;
 
     void Start () {
         exitButton = GameObject.Find("close").GetComponent<Button>();
         playButton = GameObject.Find("play").GetComponent<Button>();
+        settingsButton = GameObject.Find("settings").GetComponent<Button>();
 
-        exitButton.onClick.AddListener(() => Application.Quit());
         playButton.onClick.AddListener(() => SceneManager.LoadScene("GameScene"));
+        settingsButton.onClick.AddListener(() => SceneManager.LoadScene("SettingsScene"));
+        exitButton.onClick.AddListener(() => Application.Quit());
     }
 }
---
+++ AirBaloons/Assets/Script/ScreenLoaders/SettingsSceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SettingsSceneLoader : MonoBehaviour {
+
+    private Button exitButton;
+    private Button saveButton;
+    private Toggle systemSound;
+    private Slider volumeSlider;
+
+    void Start () {
+        exitButton = GameObject.Find("ExitButton").GetComponent<Button>();
+        exitButton.onClick.AddListener(() => SceneManager.LoadScene("MainScene"));
+
+        systemSound = GameObject.Find("switch").GetComponent<Toggle>();
+        systemSound.onValueChanged.AddListener((isOn) => AudioListener.pause = !isOn);
+
+        volumeSlider = GameObject.Find("volume_slider").GetComponent<Slider>();
+        volumeSlider.onValueChanged.AddListener((isOn) => AudioListener.volume = volumeSlider.value);
+
+
+        saveButton = GameObject.Find("SaveButton").GetComponent<Button>();
+        saveButton.onClick.AddListener(() => SceneManager.LoadScene("MainScene"));  // TODO: wire to save setting in file
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
