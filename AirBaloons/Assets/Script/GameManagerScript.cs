bba2c079e40cb19ed4e3b9d9a0800cd0403df1d8
jviaches <jviaches@gmail.com>
2019-01-20 13:21:12 -0600

    Added goal level notification at the begging of every level

--- AirBaloons/Assets/Script/GameManagerScript.cs
+++ AirBaloons/Assets/Script/GameManagerScript.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Script.Levels;
 using Assets.Script.Settings;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,6 +16,10 @@ public class GameManagerScript : MonoBehaviour {
     private Text levelTimerText;
 
     void Start () {
+
+        Time.timeScale = 0.00001f;
+        Invoke("showLevelGoalNotification", 0.00002f);
+        
         player = Instantiate((GameObject)Resources.Load("Prefabs/Actors/Player")).GetComponent<PlayerScript>(); // TODO: load from file
         currLevel = LevelSettings.GetCurrentLevel();
 
@@ -23,11 +28,17 @@ public class GameManagerScript : MonoBehaviour {
         InvokeRepeating("generateBaloons", 0f, GameSettings.BallonsGenerationFrequensy * currLevel.BaloonGenerationFrequencyModifier);
         InvokeRepeating("generateBonusPlanes", 0f, GameSettings.PlanesGenerationFrequensy * currLevel.PlaneGenerationFrequencyModifier);
     }
-	
+
+    private void showLevelGoalNotification()
+    {
+        GameObject.Find("level-goal-notification").SetActive(false);
+        Time.timeScale = 1f;
+    }
+
     private void generateBaloons()
     {
-        var random = Random.onUnitSphere * GameSettings.BornRadius; //Returns a random point on the surface of a sphere with radius 40
-        Instantiate((GameObject)Resources.Load("Prefabs/Actors/Baloon" + Random.Range(1,3)), random, Quaternion.identity);
+        var random = UnityEngine.Random.onUnitSphere * GameSettings.BornRadius; //Returns a random point on the surface of a sphere with radius 40
+        Instantiate((GameObject)Resources.Load("Prefabs/Actors/Baloon" + UnityEngine.Random.Range(1,3)), random, Quaternion.identity);
     }
 
     private void generateBonusPlanes()
