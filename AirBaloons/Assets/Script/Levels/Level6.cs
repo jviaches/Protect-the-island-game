1ae3bf112705a4c723c4d175380b40782745148a
jviaches <jviaches@gmail.com>
2019-02-05 22:33:12 -0600

    added levels classes

---
+++ AirBaloons/Assets/Script/Levels/Level6.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Script.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Script.Levels
+{
+    public class Level6 : ILevel
+    {
+        public int LevelIndex { get { return 6; } }
+
+        public float BaloonGenerationFrequencyModifier { get { return 1.2f; } }
+
+        public float PlaneGenerationFrequencyModifier { get { return 20; } }
+
+        public int MoneyGenerationModifier { get { return 1; } }
+
+        private readonly Dictionary<float, FloatItem> timeActivationDic;
+        public Dictionary<float, FloatItem> TimeActivationDic
+        {
+            get { return timeActivationDic; }
+        }
+
+        public Level6()
+        {
+            timeActivationDic = new Dictionary<float, FloatItem>();
+            timeActivationDic.Add(3, new FloatItem("Prefabs/Actors/Blimp1", new Vector3(45f, 10f, 108f)));
+        }
+    }
+}
