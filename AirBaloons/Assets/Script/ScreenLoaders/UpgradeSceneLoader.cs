e5e6ef43a10c29277529f9777cf50d42507f3ec5
jviaches <jviaches@gmail.com>
2019-02-23 14:49:38 -0600

    GUI update for Upgrade scene to distingush enebaled and disable heros + move some resources

--- AirBaloons/Assets/Script/ScreenLoaders/UpgradeSceneLoader.cs
+++ AirBaloons/Assets/Script/ScreenLoaders/UpgradeSceneLoader.cs
@@ -16,6 +16,9 @@ public class UpgradeSceneLoader : MonoBehaviour
     private Button upgradeZhuHeroButton;
     private Button upgradeZhaHeroButton;
 
+    private string ActiveHeroItem = "Graphics/universal_slot";
+    private string InActiveHeroItem = "Graphics/button_simple_inactive";
+
     private GameSettings gameSettings;
     private UpgradeSettings upgradeSettings;
 
@@ -28,16 +31,32 @@ public class UpgradeSceneLoader : MonoBehaviour
         exitButton.onClick.AddListener(() => SceneManager.LoadScene("LevelsScene"));
 
         upgradeZhoHeroButton = GameObject.Find("medium_button_zho").GetComponent<Button>();
-        upgradeZhoHeroButton.onClick.AddListener(() => gameSettings.UpgradeSettings.UpgradeHeroNextLevel(Hero.Zhouyu));
+        upgradeZhoHeroButton.onClick.AddListener(() =>
+        {
+            gameSettings.UpgradeSettings.UpgradeHeroNextLevel(Hero.Zhouyu);
+            setupHeroDetails();
+        });
 
         upgradeDaoHeroButton = GameObject.Find("medium_button_dao").GetComponent<Button>();
-        upgradeDaoHeroButton.onClick.AddListener(() => gameSettings.UpgradeSettings.UpgradeHeroNextLevel(Hero.Daochan));
+        upgradeDaoHeroButton.onClick.AddListener(() =>
+        {
+            gameSettings.UpgradeSettings.UpgradeHeroNextLevel(Hero.Daochan);
+            setupHeroDetails();
+        });
 
         upgradeZhuHeroButton = GameObject.Find("medium_button_zhu").GetComponent<Button>();
-        upgradeZhuHeroButton.onClick.AddListener(() => gameSettings.UpgradeSettings.UpgradeHeroNextLevel(Hero.Zhugeliang));
+        upgradeZhuHeroButton.onClick.AddListener(() =>
+        {
+            gameSettings.UpgradeSettings.UpgradeHeroNextLevel(Hero.Zhugeliang);
+            setupHeroDetails();
+        });
 
         upgradeZhaHeroButton = GameObject.Find("medium_button_zha").GetComponent<Button>();
-        upgradeZhaHeroButton.onClick.AddListener(() => gameSettings.UpgradeSettings.UpgradeHeroNextLevel(Hero.Zhangjiao));
+        upgradeZhaHeroButton.onClick.AddListener(() =>
+        {
+            gameSettings.UpgradeSettings.UpgradeHeroNextLevel(Hero.Zhangjiao);
+            setupHeroDetails();
+        });
 
         setupHeroDetails();
     }
@@ -49,11 +68,13 @@ public class UpgradeSceneLoader : MonoBehaviour
         {
             GameObject.Find("info_Hero Daochan").GetComponent<Text>().text = "<b>Level</b> " + daoHero.Level + "          <b>Damage:</b> " + daoHero.Damage;
             GameObject.Find("descr_HeroDaochan").GetComponent<Text>().text = daoHero.Description;
+            GameObject.Find("item_Hero Daochan").GetComponent<Image>().sprite = Resources.Load<Sprite>(ActiveHeroItem);
         }
         else
         {
             GameObject.Find("info_Hero Daochan").GetComponent<Text>().text = "<b>Level</b> 1          <b>Damage:</b> " + gameSettings.UpgradeSettings.HerosList.First(hero => hero.Hero == Hero.Daochan).Damage;
             GameObject.Find("descr_HeroDaochan").GetComponent<Text>().text = "Unlock to use";
+            GameObject.Find("item_Hero Daochan").GetComponent<Image>().sprite = Resources.Load<Sprite>(InActiveHeroItem);
         }
 
         var zhoHero = gameSettings.UpgradeSettings.PlayerHerosList.FirstOrDefault(hero => hero.Hero == Hero.Zhouyu);
@@ -61,11 +82,14 @@ public class UpgradeSceneLoader : MonoBehaviour
         {
             GameObject.Find("info_HeroZhouyu").GetComponent<Text>().text = "<b>Level</b> " + zhoHero.Level + "          <b>Damage:</b> " + zhoHero.Damage;
             GameObject.Find("descr_HeroZhouyu").GetComponent<Text>().text = daoHero.Description;
+            GameObject.Find("item_HeroZhouyu").GetComponent<Image>().sprite = Resources.Load<Sprite>(ActiveHeroItem);
         }
         else
         {
             GameObject.Find("info_HeroZhouyu").GetComponent<Text>().text = "<b>Level</b> 1          <b>Damage:</b> " + gameSettings.UpgradeSettings.HerosList.First(hero => hero.Hero == Hero.Zhouyu).Damage; 
             GameObject.Find("descr_HeroZhouyu").GetComponent<Text>().text = "Unlock to use";
+            GameObject.Find("item_HeroZhouyu").GetComponent<Image>().sprite = Resources.Load<Sprite>(InActiveHeroItem);
+            
         }
 
         var zhaHero = gameSettings.UpgradeSettings.PlayerHerosList.FirstOrDefault(hero => hero.Hero == Hero.Zhangjiao);
@@ -73,11 +97,13 @@ public class UpgradeSceneLoader : MonoBehaviour
         {
             GameObject.Find("info_Hero Zhangjiao").GetComponent<Text>().text = "<b>Level</b> " + zhaHero.Level + "          <b>Damage:</b> " + zhaHero.Damage;
             GameObject.Find("descr_HeroZhangjiao").GetComponent<Text>().text = daoHero.Description;
+            GameObject.Find("item_Hero Zhangjiao").GetComponent<Image>().sprite = Resources.Load<Sprite>(ActiveHeroItem);
         }
         else
         {
             GameObject.Find("info_Hero Zhangjiao").GetComponent<Text>().text = "<b>Level</b> 1          <b>Damage:</b> " + gameSettings.UpgradeSettings.HerosList.First(hero => hero.Hero == Hero.Zhangjiao).Damage;
             GameObject.Find("descr_HeroZhangjiao").GetComponent<Text>().text = "Unlock to use";
+            GameObject.Find("item_Hero Zhangjiao").GetComponent<Image>().sprite = Resources.Load<Sprite>(InActiveHeroItem);
         }
 
         var zhuHero = gameSettings.UpgradeSettings.PlayerHerosList.FirstOrDefault(hero => hero.Hero == Hero.Zhugeliang);
@@ -85,11 +111,13 @@ public class UpgradeSceneLoader : MonoBehaviour
         {
             GameObject.Find("info_Hero Zhugeliang").GetComponent<Text>().text = "<b>Level</b> " + zhuHero.Level + "          <b>Damage:</b> " + zhuHero.Damage;
             GameObject.Find("descr_HeroZhugeliang").GetComponent<Text>().text = daoHero.Description;
+            GameObject.Find("item_Hero Zhugeliang").GetComponent<Image>().sprite = Resources.Load<Sprite>(ActiveHeroItem);
         }
         else
         {
             GameObject.Find("info_Hero Zhugeliang").GetComponent<Text>().text = "<b>Level</b> 1          <b>Damage:</b> " + gameSettings.UpgradeSettings.HerosList.First(hero => hero.Hero == Hero.Zhugeliang).Damage;
             GameObject.Find("descr_HeroZhugeliang").GetComponent<Text>().text = "Unlock to use";
+            GameObject.Find("item_Hero Zhugeliang").GetComponent<Image>().sprite = Resources.Load<Sprite>(InActiveHeroItem);
         }
     }
 }
