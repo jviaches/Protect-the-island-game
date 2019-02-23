b0152c25e15b116b3bf61400375d53821e7fa2c5
jviaches <jviaches@gmail.com>
2019-02-23 11:18:59 -0600

    Added levels for upgraded heros

--- AirBaloons/Assets/Script/ScreenLoaders/UpgradeSceneLoader.cs
+++ AirBaloons/Assets/Script/ScreenLoaders/UpgradeSceneLoader.cs
@@ -1,5 +1,7 @@
 ﻿using Assets.Script.Settings;
+using System;
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -36,5 +38,38 @@ public class UpgradeSceneLoader : MonoBehaviour
 
         upgradeZhaHeroButton = GameObject.Find("medium_button_zha").GetComponent<Button>();
         upgradeZhaHeroButton.onClick.AddListener(() => gameSettings.UpgradeSettings.UpgradeHeroNextLevel(Hero.Zhangjiao));
+
+        setupHeroDetails();
+    }
+
+    private void setupHeroDetails()
+    {
+        var daoHero = gameSettings.UpgradeSettings.PlayerHerosList.FirstOrDefault(hero => hero.Hero == Hero.Daochan);
+        if (daoHero != null)
+            GameObject.Find("info_Hero Daochan").GetComponent<Text>().text = "<b>Level</b> " + daoHero.Level +"          <b>Damage:</b> " + daoHero.Damage;
+        else
+            GameObject.Find("info_Hero Daochan").GetComponent<Text>().text = "<b>Level</b> 0          <b>Damage:</b> ---";
+
+
+        var zhoHero = gameSettings.UpgradeSettings.PlayerHerosList.FirstOrDefault(hero => hero.Hero == Hero.Zhouyu);
+        if (zhoHero != null)
+            GameObject.Find("info_HeroZhouyu").GetComponent<Text>().text = "<b>Level</b> " + zhoHero.Level + "          <b>Damage:</b> " + zhoHero.Damage;
+        else
+            GameObject.Find("info_HeroZhouyu").GetComponent<Text>().text = "<b>Level</b> 0          <b>Damage:</b> ---";
+
+
+        var zhaHero = gameSettings.UpgradeSettings.PlayerHerosList.FirstOrDefault(hero => hero.Hero == Hero.Zhangjiao);
+        if (zhaHero != null)
+            GameObject.Find("info_Hero Zhangjiao").GetComponent<Text>().text = "<b>Level</b> " + zhaHero.Level + "          <b>Damage:</b> " + zhaHero.Damage;
+        else
+            GameObject.Find("info_Hero Zhangjiao").GetComponent<Text>().text = "<b>Level</b> 0          <b>Damage:</b> ---";
+
+
+        var zhuHero = gameSettings.UpgradeSettings.PlayerHerosList.FirstOrDefault(hero => hero.Hero == Hero.Zhugeliang);
+        if (zhuHero != null)
+            GameObject.Find("info_Hero Zhugeliang").GetComponent<Text>().text = "<b>Level</b> " + zhuHero.Level + "          <b>Damage:</b> " + zhuHero.Damage;
+        else
+            GameObject.Find("info_Hero Zhugeliang").GetComponent<Text>().text = "<b>Level</b> 0          <b>Damage:</b> ---";
     }
 }
+
